8b62a33c1edec041db606f44bc8e4c3a8acf8565
barbodnavabi <barbodnvb@gmail.com>
2021-04-05 03:01:20 -0700

    start for create lastest job in detail view

--- jobs/views.py
+++ jobs/views.py
@@ -51,6 +51,7 @@ class JobsDetailView(FormMixin,DetailView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['lastest'] = Jobs.objects.filter(status='p').order_by('-date')[:16]
         return context
     
 
