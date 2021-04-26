01c2a97792e5e94e59f5ada00de33ee629f7b74e
barbodnavabi <barbodnvb@gmail.com>
2021-04-26 02:37:43 -0700

    start resume create views

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView
+
+
+class UserInfoCreateView(CreateView):
+    model = UserInfo
+    template_name = "resume/user_info_create.html"
+
+
+class StudyCreateView(CreateView):
+    model = Study
+    template_name = "study_create.html"
+
+
+
+class SkillCreateView(CreateView):
+    model = Skill
+    template_name = "skill_create.html"
+
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = "project_create.html"
+
+
+class ExperienceCreateView(CreateView):
+    model = Experience
+    template_name = "experience_create.html"
