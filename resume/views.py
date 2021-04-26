202f8c44f370247e0f4d665008b8e11c454dc9d5
barbodnavabi <barbodnvb@gmail.com>
2021-04-26 03:37:18 -0700

    create view suucess_urls

--- resume/views.py
+++ resume/views.py
@@ -1,31 +1,36 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,reverse
 from django.views.generic import CreateView
 
 
 class UserInfoCreateView(CreateView):
     model = UserInfo
     template_name = "resume/user_info_create.html"
+    success_url = reverse('study-create')
 
 
 class StudyCreateView(CreateView):
     model = Study
     template_name = "study_create.html"
+    success_url = reverse('experience-create')
+
 
 
 
 class SkillCreateView(CreateView):
     model = Skill
     template_name = "skill_create.html"
+    success_url = reverse('project-create')
 
 
 
 class ProjectCreateView(CreateView):
     model = Project
     template_name = "project_create.html"
+    success_url = reverse('skill-create')
 
 
 class ExperienceCreateView(CreateView):
     model = Experience
     template_name = "experience_create.html"
+    success_url=reverse('index')
+
